981fbffedec8c37e25725f5828920fff740cbcd7
marvel-ironman <marvelmartsev@gmail.com>
2019-10-08 15:16:54 +0300

    with waits

--- src/main/java/TestWait.java
+++ src/main/java/TestWait.java
@@ -1,6 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestWait {
     public static void main(String[] args) {
@@ -10,6 +16,24 @@ public class TestWait {
 
         driver = new ChromeDriver();
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.get("https://www.google.com.ua/");
+
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("hplogo")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("MiYK0e")));
+
+        wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver){
+                System.out.println(driver.getTitle());
+                return  driver.getTitle().startsWith("Goo");
+            }
+        });
+
+
+
     }
 }
