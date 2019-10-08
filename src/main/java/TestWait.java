61f23256d4d664e07a5a5ec62646bf30e04d61e9
marvel-ironman <marvelmartsev@gmail.com>
2019-10-08 10:57:41 +0300

    create project

---
+++ src/main/java/TestWait.java
@@ -0,0 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestWait {
+    public static void main(String[] args) {
+
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.get("https://www.google.com.ua/");
+    }
+}
